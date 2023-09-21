af83373615c1f0b7e4ca70888125c681623e5034
Daniel <mwdchang@gmail.com>
2023-09-20 22:32:15 -0400

    tweaks

--- packages/client/src/views/AboutView.vue
+++ packages/client/src/views/AboutView.vue
@@ -2,38 +2,54 @@
   <main class="about" v-if="mageStore.mage">
     <h2>Kingdom {{ mageStore.mage.name }} (# {{mageStore.mage.id}})</h2>
     <br>
-    <br>
-    <section>
-      <div class="row">
-        <div>Land</div><div>{{ totalLand(mageStore.mage) }} </div>
-      </div>
-      <div class="row">
-        <div>Fortress</div><div>{{ mageStore.mage.fortresses }} </div>
-      </div>
-      <div class="row">
-        <div>Geld</div><div>{{ mageStore.mage.currentGeld }} </div>
-      </div>
-      <div class="row">
-        <div>Population</div><div>{{ mageStore.mage.currentPopulation}} / {{ interior.maxPopulation(mageStore.mage) }}</div>
-      </div>
-      <div class="row">
-        <div>Magic</div><div>{{ mageStore.mage.currentMana}} / {{ manaStorage(mageStore.mage) }}</div>
-      </div>
-      <div class="row">
-        <div>Items</div><div>0</div>
-      </div>
-    </section>
 
-    <router-link to="/battle">Battle</router-link>
-    <router-link to="/spell">Spells</router-link>
-    <router-link to="/rankList">Ranks</router-link>
+
+    <main style="display: flex; flex-direction: row">
+      <section>
+        <div class="row">
+          <div>Magic</div><div>{{ mageStore.mage.currentMana}} / {{ manaStorage(mageStore.mage) }}</div>
+        </div>
+        <div class="row">
+          <div>Items</div><div>0</div>
+        </div>
+
+        <br>
+        <router-link to="/spell">Spells</router-link>
+      </section>
+      <section>
+        <img v-if="mageStore.mage.magic==='ascendant'" src="@/assets/images/ascendant.jpeg" />
+        <img v-if="mageStore.mage.magic==='verdant'" src="@/assets/images/verdant.jpeg" />
+        <img v-if="mageStore.mage.magic==='eradication'" src="@/assets/images/eradication.jpeg" />
+        <img v-if="mageStore.mage.magic==='nether'" src="@/assets/images/netehr.jpeg" />
+        <img v-if="mageStore.mage.magic==='phantasm'" src="@/assets/images/phantasm.jpeg" />
+      </section>
+      <section>
+        <div class="row">
+          <div>Land</div><div>{{ totalLand(mageStore.mage) }} </div>
+        </div>
+        <div class="row">
+          <div>Fortress</div><div>{{ mageStore.mage.fortresses }} </div>
+        </div>
+        <div class="row">
+          <div>Geld</div><div>{{ mageStore.mage.currentGeld }} </div>
+        </div>
+        <div class="row">
+          <div>Population</div><div>{{ mageStore.mage.currentPopulation}} / {{ interior.maxPopulation(mageStore.mage) }}</div>
+        </div>
+
+        <br>
+        <router-link to="/interior">Interior</router-link>
+        <div> 
+          <router-link to="/build">Building</router-link> 
+          <span class="spacer"/>
+          <router-link to="/destroy">Destroy</router-link> 
+        </div>
+      </section>
+    </main>
+
     <router-link to="/status">Status Report</router-link>
-    <router-link to="/interior">Interior</router-link>
-    <div> 
-      <router-link to="/build">Building</router-link> 
-      <span class="spacer"/>
-      <router-link to="/destroy">Destroy</router-link> 
-    </div>
+    <router-link to="/battle">Battle</router-link>
+    <router-link to="/rankList">Ranks</router-link>
     <div> 
       <!--
       <router-link to="/build">Recruit</router-link> 
@@ -50,6 +66,7 @@ import { useMageStore } from '@/stores/mage';
 import * as interior from 'engine/src/interior';
 import { totalLand } from 'engine/src/base/mage';
 import { manaStorage } from 'engine/src/magic';
+
 const mageStore = useMageStore();
 
 </script>
