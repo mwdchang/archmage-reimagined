2e1bf03d592ad50364368069a53fdc7f0727e818
Daniel Chang <mwdchang@gmail.com>
2025-09-09 23:49:50 -0400

    Battle plumbing (#127)

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -76,13 +76,7 @@ onMounted(async () => {
 
   loadItemData(lesserItems);
 
-  /*
   try {
-    console.log('!!!!!', route.name);
-    if (route.name === 'guide') {
-      return;
-    }
-
     const r = await API.get('mage');
     mageStore.setLoginStatus(1);
     mageStore.setMage(r.data.mage);
@@ -91,13 +85,14 @@ onMounted(async () => {
       router.push({ name: 'about' });
     }
   } catch (e: any) {
-    console.log('doh!!!');
     if (e.response.status === 403 || e.response.status === 401) {
       mageStore.setLoginStatus(0);
+      if (publicRoutes.includes(route.name as string)) {
+        return
+      }
       router.push({ name: 'home' });
     }
   }
-  */
 });
 
 </script>
