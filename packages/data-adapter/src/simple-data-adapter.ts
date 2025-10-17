37e1a5685b3cd30a2f3ebaf5da630828dad79754
Daniel Chang <mwdchang@gmail.com>
2025-10-16 23:34:40 -0400

    item heal

--- packages/data-adapter/src/simple-data-adapter.ts
+++ packages/data-adapter/src/simple-data-adapter.ts
@@ -43,7 +43,7 @@ export class SimpleDataAdapter extends DataAdapter {
     endTurn: 0
   }
 
-  mageSeq: 0;
+  mageSeq:number = 0;
 
   constructor() { super(); }
 
@@ -109,7 +109,8 @@ export class SimpleDataAdapter extends DataAdapter {
   }
 
   async nextMageId(): Promise<number> {
-    return ++this.mageSeq;
+    this.mageSeq++;
+    return this.mageSeq;
   }
 
   async createMage(username: string, mage: Mage) {
--- packages/engine/src/war.ts
+++ packages/engine/src/war.ts
@@ -497,7 +497,11 @@ const battleItem = (
           applyUnitEffect(effectOrigin, unitAttrEffect, affectedArmy);
         } else if (eff.effectType === 'UnitDamageEffect') {
           const damageEffect = eff as UnitDamageEffect;
-          applyDamageEffect(effectOrigin, damageEffect, affectedArmy);
+          const damageLogs = applyDamageEffect(effectOrigin, damageEffect, affectedArmy);
+          logs.push(...damageLogs);
+        } else if (eff.effectType === 'UnitHealEffect') {
+          const healEffect = eff as UnitHealEffect;
+          applyHealEffect(effectOrigin, healEffect, affectedArmy);
         } else if (eff.effectType === 'TemporaryUnitEffect') {
           const tempUnitEffect = eff as TemporaryUnitEffect;
           const newStack = applyTemporaryUnitEffect(effectOrigin, tempUnitEffect, caster.mage);
@@ -790,7 +794,6 @@ export const battle = (battleType: string, attacker: Combatant, defender: Combat
       }
 
       if (unitLoss > 0) {
-        console.log('!!!', stack.unit.id, unitLoss)
         stack.sustainedDamage = 0;
       }
       stack.sustainedDamage += (totalDamage % stack.unit.hitPoints);
@@ -816,7 +819,6 @@ export const battle = (battleType: string, attacker: Combatant, defender: Combat
       }
 
       if (unitLoss > 0) {
-        console.log('!!!', stack.unit.id, unitLoss)
         stack.sustainedDamage = 0;
       }
       stack.sustainedDamage += (totalDamage % stack.unit.hitPoints);
@@ -871,7 +873,6 @@ export const battle = (battleType: string, attacker: Combatant, defender: Combat
         const burstingAbilities = dUnit.abilities.filter(d => d.name === 'bursting');
 
         for (const burstingAbility of burstingAbilities) {
-          console.log('handle burst', burstingAbility);
 
           // default
           let burstingType = dUnit.primaryAttackType;
