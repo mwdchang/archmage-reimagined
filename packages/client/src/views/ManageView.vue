560fd0df1f8f82be893dc9c74fafe7390ea8051d
Daniel Chang <mwdchang@gmail.com>
2025-11-22 17:25:08 -0500

    option/message stub

---
+++ packages/client/src/views/ManageView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-tabs">
+    <div class="tab" :class="{ active: tabView === 'message' }" @click="changeView('message')">Messages</div>
+    <div class="tab" :class="{ active: tabView === 'option' }" @click="changeView('option')">Options</div>
+  </div>
+
+  Coming soon ...
+
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const tabView = ref('message');
+
+const changeView = (v: string) => {
+  tabView.value = v;
+};
+
+</script>
+
+
